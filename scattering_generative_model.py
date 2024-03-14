b3f7551a6cc8da28a71748e9577f52f9d33fa433
tk-araki <64360195+tk-araki@users.noreply.github.com>
2024-03-14 12:24:14 +0900

    delete superfluous comments

--- scattering_generative_model.py
+++ scattering_generative_model.py
@@ -16,11 +16,11 @@ from torch.utils.tensorboard import SummaryWriter #  tensorboard --logdir=runs -
 from datasets import ImageScatDataset
 from generator import ImageGenerator
 
-class ScatteringGenerativeModel:# ScatteringGenerativeModel
+class ScatteringGenerativeModel:
 
     def __init__(self,
                  model_def_params=None,
-                 model_file=None): # configs  # load_optimizer = None, # cpu_only = False
+                 model_file=None):
         '''
         : dictionary
             key:scat_dim image_size num_final_channels input_size(arbitrary)
@@ -30,35 +30,22 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
             raise ValueError("No argument. Specify one argument, 'model_def_params' or 'model_file'.")
         if model_def_params is not None and model_file is not None:
             raise ValueError("Two arguments. Specify only one argument, 'model_def_params' or 'model_file'.")
-        '''
-                self.study_name = study_name
-                if direction is None and directions is None:
-                    raise ("Specify one of `direction` and `directions`.")
-                elif directions is not None:
-                    self._directions = list(directions)
-                elif direction is not None:
-                    self._directions = [direction]
-                else:
-                    raise ValueError("Specify only one of `direction` and `directions`.")
-        '''
+
         self.cur_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.optimizer = None
         if model_def_params is not None:
             self.image_generator = ImageGenerator(**model_def_params)
             self.model_def_params = MappingProxyType(model_def_params.copy())
-            # self.test = model_def_params
             self.epoch = 0
 
         if model_file is not None:
-            # with open(model_file.parent / 'model_def_params.json') as f:
-            #     model_def_params = json.load(f)
             checkpoint = torch.load(model_file)# torch.load(model_file,map_location=torch.device('cpu'))
             self.epoch = checkpoint['epoch']
             self.model_def_params = MappingProxyType(checkpoint['model_def_params'])
             self.image_generator = ImageGenerator(**self.model_def_params)
 
             self.image_generator.load_state_dict(checkpoint['model_state_dict'])
-            # optimizerあったらロード
+
             if 'optimizer_state_dict' in checkpoint.keys():
                 self.optimizer = optim.Adam(self.image_generator.parameters())
                 self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -69,12 +56,6 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
                             state[k] = v.to(self.cur_device)
 
         self.image_generator.to(self.cur_device)
-
-    # def load_optimizer(self,optim_path = None):
-    # def create_optimizer(self,optim_path = None):
-    #     if optim_path is None:
-    #         optim.Adam(self.image_generator.parameters())
-    # def configure_optimizers(self):
 
     def __update_image_generator(self,dataloader_train,loss):# __update_generator_params
         if not self.image_generator.training:
@@ -88,7 +69,7 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
             self.optimizer.zero_grad()
             recons_batch = self.image_generator(scat_batch)
             loss_recons = loss(image_batch, recons_batch)
-            loss_recons.backward()# list(self.image_generator.parameters())[0].grad; list(self.image_generator.parameters())[0][0,0]
+            loss_recons.backward()
 
             self.optimizer.step()
 
@@ -109,15 +90,7 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
 
         return mean_loss / num_data
 
-    # def get_model_def_params(self):
-    #     return {
-    #         'scat_dim': self.image_generator.generator_input.in_features,
-    #         'image_size': self.image_generator.input_tensor_size[1] * 2 ** self.image_generator.level_uppsampling,
-    #         'num_final_channels': self.image_generator.final_layer[0].in_channels,
-    #         'input_size': self.image_generator.input_tensor_size[1],
-    #     }
-
-    def save_checkpoint(self,file_path,epoch,batch_size,train_loss,valid_loss,save_optimizer = True): # .pth
+    def save_checkpoint(self,file_path,epoch,batch_size,train_loss,valid_loss,save_optimizer = True):
         state = {"epoch": epoch,
                  "model_def_params":dict(self.model_def_params),
                  "batch_size":batch_size,
@@ -131,17 +104,13 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
 
     def train(self, num_epochs, batch_size, train_scat_dir, train_image_dir,
               valid_scat_dir,valid_image_dir,
-              save_dir=Path('model_checkpoints'),  # model_save_dir = ^^^
+              model_save_dir=Path('model_checkpoints'),
               log_dir=Path('logs_training'),
               model_save_interval=None, valid_log_interval = 1,
               pin_memory=True, num_workers=2,lr_adam = None):
-        '''
-        run_all.py
-        batch_size=train_batch_size; train_scat_dir = path_set['train_scatpcs_dir']; save_dir=path_set['trained_model_dir']
-        '''
 
-        if not save_dir.exists():
-            save_dir.mkdir(parents=True)
+        if not model_save_dir.exists():
+            model_save_dir.mkdir(parents=True)
 
         if not log_dir.exists():
             log_dir.mkdir(parents=True)
@@ -152,25 +121,24 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
         if self.optimizer is not None and lr_adam is not None:
             print("'lr_adam' is not used since self.optimizer already exists")# Argument warnings.warn(
 
-        # 学習用dataloader作成
+        # dataloader for training
         train_imgscat_dataset = ImageScatDataset(train_image_dir,train_scat_dir,scat_suffix='_scat_pcs.npy') # scat_suffix モジュールファイルのグローバル変数にする？
         train_imgscat_dataloader = DataLoader(train_imgscat_dataset, batch_size, pin_memory=pin_memory, num_workers=num_workers)
 
-        # validation用dataloader作成
+        # dataloader for validation
         valid_imgscat_dataset = ImageScatDataset(valid_image_dir,valid_scat_dir,scat_suffix='_scat_pcs.npy') # scat_suffix モジュールファイルのグローバル変数にする？
         valid_imgscat_dataloader = DataLoader(valid_imgscat_dataset, batch_size, pin_memory=pin_memory, num_workers=num_workers)
 
-        l1_loss = torch.nn.L1Loss() # l1_loss
+        l1_loss = torch.nn.L1Loss()
 
-        if self.optimizer is None: # , optim_params = {} ↓ ( , optim_params)
-            if lr_adam is None: # lr_adam = 1e-3 if lr_adam is None else lr_adam
+        if self.optimizer is None:
+            if lr_adam is None:
                 lr_adam = 1e-3
             self.optimizer = optim.Adam(self.image_generator.parameters(),lr=lr_adam)
 
-        writer_training = SummaryWriter(log_dir) # フォルダがないときは作成される
+        writer_training = SummaryWriter(log_dir)
 
         for epoch in trange(self.epoch,self.epoch + num_epochs+1): # 後で変更予定
-            # torch.cuda.synchronize()
 
             # train
             if epoch != self.epoch:
@@ -196,16 +164,16 @@ class ScatteringGenerativeModel:# ScatteringGenerativeModel
 
             # model save
             if epoch != self.epoch and not epoch % model_save_interval:
-                self.save_checkpoint(save_dir / f'model_at_{epoch}epoch.pth', epoch,batch_size,
+                self.save_checkpoint(model_save_dir / f'model_at_{epoch}epoch.pth', epoch,batch_size,
                                      train_loss,valid_loss, save_optimizer=True)
 
         writer_training.close()
         self.epoch += num_epochs
 
 
-    # レビュー対象　return 部分   名前generate_image_grid  grid of images
+    # generate_image_grid
     def generate_images(self,scat_batch,num_cols=8,save_file=None
-                        ):  # train_scat_dir=None,scat_batch=None ;; scat_coefs
+                        ):
         if self.image_generator.training:
             self.image_generator.eval()
 
